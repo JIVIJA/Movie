03361305adff6fbed48c32a456f574c7ddc061d3
mac-0007 <mac-0007@gmail.com>
2019-01-22 12:16:09 +0530

    Initial Commit

---
+++ Movie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movie
+//
+//  Created by mac-0007 on 22/01/19.
+//  Copyright © 2019 mac-0007. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
